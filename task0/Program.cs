a3cbbdd41c05024406d60c345f3209ca61d8c112
Roman <romankos1993@gmail.com>
2022-09-30 21:51:16 +0300

    task 0 проблема

--- task0/Program.cs
+++ task0/Program.cs
@@ -1,6 +1,8 @@
 ﻿// 1. программа считает квадрат числа
 
 Console.Write("Введите число: ");
+
 int number = int.Parse(Console.ReadLine());
+
 int sqr = number * number;
 Console.WriteLine("Квадрат числа" + sqr);
