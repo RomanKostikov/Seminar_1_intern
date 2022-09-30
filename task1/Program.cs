2da43db7b47eca07bcb0b807789685ee124c7165
Roman <romankos1993@gmail.com>
2022-09-30 21:27:11 +0300

    task 1 conplete

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,7 +4,15 @@
 // a = 9, b = -3 -> да 
 // a = -3 b = 9 -> нет
 
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-int sqr = number * number;
-Console.WriteLine("Квадрат числа" + sqr);
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine());
+if(number2 == number1*number1)
+{
+    Console.WriteLine("Число 2 является квадратом числа 1.");
+}
+else
+{
+    Console.WriteLine("Число 2 не является квадратом числа 1.");
+}
